09d071cee197958df86eb95d87bf6b4052e9da13
Jacob Ebey <jacob.ebey@live.com>
2022-05-14 03:17:23 -0700

    updated lint config

--- config/eslint-config-acme/index.js
+++ config/eslint-config-acme/index.js
@@ -1,3 +1,10 @@
-module.exports = {
+/** @type {import("eslint/conf/eslint-all")} */
+let config = {
   extends: ["@remix-run/eslint-config", "prettier"],
+  ignorePatterns: ["node_modules", "build"],
+  settings: {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+  }
 };
+
+module.exports = config;
